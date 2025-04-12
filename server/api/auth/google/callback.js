e641bb342759af41f416249cae72ef4d462d8828
Den Kong <denusklo@gmail.com>
2025-04-12 14:31:57 +0800

    fix: temporarily disable Google auth callback for debugging config values

--- server/api/auth/google/callback.js
+++ server/api/auth/google/callback.js
@@ -4,97 +4,100 @@ import { google } from 'googleapis'
 export default defineEventHandler(async (event) => {
 
   const query = getQuery(event)
-
-  // console.log(query);
-
-
-  // return {
-  //   message: query,
-  // };
-
-
-  const code = query.code
   const config = useRuntimeConfig()
 
-  if (!code) {
-    return sendRedirect(event, `/bookings/auth-error?error=${encodeURIComponent('Authorization code is missing')}`)
-  }
+  return {
+    'config.googleClientId': config.googleClientId,
+    'config.googleClientSecret': config.googleClientSecret,
+    'config.googleRedirectUri': config.googleRedirectUri,
+  };
 
-  const oauth2Client = new google.auth.OAuth2(
-    config.googleClientId,
-    config.googleClientSecret,
-    config.googleRedirectUri
-  )
 
-  try {
-    const { tokens } = await oauth2Client.getToken(code)
+  // const code = query.code
+  // const config = useRuntimeConfig()
 
-    // Instead of redirecting, return an HTML page that sends a message to the opener
-    const html = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Authorization Successful</title>
-        <script>
-          // Send tokens back to the opener window
-          if (window.opener) {
-            window.opener.postMessage({
-              type: 'google-auth-success',
-              accessToken: '${tokens.access_token}',
-              refreshToken: '${tokens.refresh_token || ''}',
-              expiryDate: '${tokens.expiry_date}'
-            }, '*');
+  // if (!code) {
+  //   return sendRedirect(event, `/bookings/auth-error?error=${encodeURIComponent('Authorization code is missing')}`)
+  // }
 
-            // Close this window after sending data
-            setTimeout(() => window.close(), 1000);
-          } else {
-            document.body.innerHTML = '<p>Authorization successful. You can close this window and return to the application.</p>';
-          }
-        </script>
-      </head>
-      <body>
-        <p>Authorization successful! This window will close automatically...</p>
-      </body>
-      </html>
-    `;
+  // const oauth2Client = new google.auth.OAuth2(
+  //   config.googleClientId,
+  //   config.googleClientSecret,
+  //   config.googleRedirectUri
+  // )
 
-    return new Response(html, {
-      headers: {
-        'Content-Type': 'text/html',
-      },
-    });
-  } catch (error) {
+  // try {
+  //   const { tokens } = await oauth2Client.getToken(code)
 
-    console.log('Detailed token exchange error:', error.response?.data || error.message || error)
+  //   // Instead of redirecting, return an HTML page that sends a message to the opener
+  //   const html = `
+  //     <!DOCTYPE html>
+  //     <html>
+  //     <head>
+  //       <title>Authorization Successful</title>
+  //       <script>
+  //         // Send tokens back to the opener window
+  //         if (window.opener) {
+  //           window.opener.postMessage({
+  //             type: 'google-auth-success',
+  //             accessToken: '${tokens.access_token}',
+  //             refreshToken: '${tokens.refresh_token || ''}',
+  //             expiryDate: '${tokens.expiry_date}'
+  //           }, '*');
 
-    // Return an error HTML page
-    const errorMsg = encodeURIComponent(JSON.stringify(error.response?.data || error.message));
-    const html = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Authorization Failed</title>
-        <script>
-          if (window.opener) {
-            window.opener.postMessage({
-              type: 'google-auth-error',
-              error: '${errorMsg}'
-            }, '*');
-            setTimeout(() => window.close(), 1000);
-          }
-        </script>
-      </head>
-      <body>
-        <p>Authorization failed. This window will close automatically...</p>
-        <p>Error: '${error.response?.data}'</p>
-      </body>
-      </html>
-    `;
+  //           // Close this window after sending data
+  //           setTimeout(() => window.close(), 1000);
+  //         } else {
+  //           document.body.innerHTML = '<p>Authorization successful. You can close this window and return to the application.</p>';
+  //         }
+  //       </script>
+  //     </head>
+  //     <body>
+  //       <p>Authorization successful! This window will close automatically...</p>
+  //     </body>
+  //     </html>
+  //   `;
 
-    return new Response(html, {
-      headers: {
-        'Content-Type': 'text/html',
-      },
-    });
-  }
+  //   return new Response(html, {
+  //     headers: {
+  //       'Content-Type': 'text/html',
+  //     },
+  //   });
+  // } catch (error) {
+
+  //   console.log('Detailed token exchange error:', error.response?.data || error.message || error)
+
+  //   // Return an error HTML page
+  //   const errorMsg = encodeURIComponent(JSON.stringify(error.response?.data || error.message));
+  //   const html = `
+  //     <!DOCTYPE html>
+  //     <html>
+  //     <head>
+  //       <title>Authorization Failed</title>
+  //       <script>
+  //         if (window.opener) {
+  //           window.opener.postMessage({
+  //             type: 'google-auth-error',
+  //             error: '${errorMsg}'
+  //           }, '*');
+  //           setTimeout(() => window.close(), 1000);
+  //         }
+  //       </script>
+  //     </head>
+  //     <body>
+  //       <p>Authorization failed. This window will close automatically...</p>
+  //       <p>Error: '${JSON.stringify(error.response?.data)}'</p>
+  //       <p>config.googleClientId: '${config.googleClientId}'</p>
+  //       <p>config.googleClientSecret: '${config.googleClientSecret}'</p>
+  //       <p>config.googleRedirectUri: '${config.googleRedirectUri}'</p>
+  //     </body>
+  //     </html>
+  //   `;
+
+  //   return new Response(html, {
+  //     headers: {
+  //       'Content-Type': 'text/html',
+  //     },
+  //   });
+  // }
 })
